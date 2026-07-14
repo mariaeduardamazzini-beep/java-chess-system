8fad15a134193ca562d16ce019becf6ca462cf94
Maria Eduarda Mazzini <mariaeduarda.omazzini@gmail.com>
2026-07-14 10:01:20 -0300

    Implementing possible moves of Rook

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,45 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] v_temp = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position position = new Position(0,0);
+
+        //above
+        position.setValue(position.getRow() - 1 , position.getColumn());
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+            position.setRow(position.getRow() - 1);
+        } if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+        }
+
+        //left
+        position.setValue(position.getRow(), position.getColumn() -1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+            position.setColumn(position.getColumn() - 1);
+        } if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+        }
+
+        //right
+        position.setValue(position.getRow(), position.getColumn() -1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+            position.setColumn(position.getColumn() + 1);
+        } if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+        }
+
+        //below
+        position.setValue(position.getRow() - 1 , position.getColumn());
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+            position.setRow(position.getRow() + 1);
+        } if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            v_temp[position.getRow()][position.getColumn()] = true;
+        }
+
         return v_temp;
     }
 }
