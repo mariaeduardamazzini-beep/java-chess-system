ce7bd17b3d44389cd967e559a3fa1c409ad0f71f
Maria Eduarda Mazzini <mariaeduarda.omazzini@gmail.com>
2026-07-14 10:44:46 -0300

    Switching player each turn

--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -1,6 +1,7 @@
 package application;
 
 import boardgame.Piece;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -49,6 +50,14 @@ public class UI {
         throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
     }
 }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " +  chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+
     public static void printBoard(ChessPiece[][] pieces)
     {
         for (int i = 0; i < pieces.length; i++) {
@@ -76,13 +85,11 @@ public class UI {
             System.out.println(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
-            System.out.print("- "+ ANSI_RESET);
-        }
-        else {
+            System.out.print("- " + ANSI_RESET);
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
             System.out.print(" ");
